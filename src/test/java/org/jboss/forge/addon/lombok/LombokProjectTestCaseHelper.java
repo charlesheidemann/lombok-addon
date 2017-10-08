5e24bf970672521c5b196401260a3ec8695f9116
Charles Heidemann <charles.heidemann@gmail.com>
2017-10-08 01:36:59 -0300

    add closable

--- src/test/java/org/jboss/forge/addon/lombok/LombokProjectTestCaseHelper.java
+++ src/test/java/org/jboss/forge/addon/lombok/LombokProjectTestCaseHelper.java
@@ -2,6 +2,7 @@ package org.jboss.forge.addon.lombok;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 
 import javax.enterprise.inject.Produces;
@@ -33,7 +34,10 @@ public class LombokProjectTestCaseHelper
       Resource<File> unreified = resourceFactory.create(JavaResource.class, tmpFile);
       JavaResource resource = unreified.reify(JavaResource.class);
 
-      resource.setContents(getClass().getResource("Person.java").openStream());
+      try (InputStream openStream = getClass().getResource("Person.java").openStream())
+      {
+         resource.setContents(openStream);
+      }
 
       return resource;
    }
